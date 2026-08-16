34a780990d2c9aa422229bb432a3079ff4101726
Ololade <73138104+zololade@users.noreply.github.com>
2026-08-16 19:44:27 +0100

    feat: add shuffle functionality to CardPiece and GameBoard components

--- src/components/CardPiece.tsx
+++ src/components/CardPiece.tsx
@@ -2,13 +2,19 @@ import { useState, type MouseEvent } from "react";
 
 import type { GameBoardProp } from "./GameBoard";
 
-function CardPiece({ updateCurrScore, resetGame, val }: GameBoardProp & { val: number }) {
+function CardPiece({
+  updateCurrScore,
+  resetGame,
+  val,
+  shuffle,
+}: GameBoardProp & { val: number; shuffle: () => void }) {
   const [clickedState, setClickedState] = useState(false);
 
   function handleClick(_e: MouseEvent) {
     if (clickedState === false) {
       setClickedState(true);
       updateCurrScore();
+      shuffle();
       return;
     }
 
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CardPiece } from "./CardPiece";
 
 interface GameBoardProp {
@@ -5,14 +7,35 @@ interface GameBoardProp {
   resetGame: () => void;
 }
 
+let myArray = Array.from({ length: 10 }).map((_val, index) => {
+  return { val: index, id: `key${index}` };
+});
+
 function GameBoard(props: GameBoardProp) {
+  const [pics, setPics] = useState([...myArray]);
+
+  function handleShuffle() {
+    setPics((prev) => {
+      return [...shuffleArray(prev)];
+    });
+  }
+
   return (
     <section className="gameBoard">
-      {Array.from({ length: 10 }).map((_val, i) => (
-        <CardPiece {...props} key={i} val={i} />
+      {pics.map((val) => (
+        <CardPiece {...props} key={val.id} val={val.val} shuffle={handleShuffle} />
       ))}
     </section>
   );
+}
+
+//helper
+function shuffleArray<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 export { GameBoard, type GameBoardProp };
